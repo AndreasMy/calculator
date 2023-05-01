8327fc59f4c40918da4c64d409f77396f7bb678d
AndreasMy <amykle85@gmail.com>
2023-05-01 11:16:40 +0200

    adding messy code and preparing to branch

--- script.js
+++ script.js
@@ -1,64 +1,73 @@
-const operationButtons = document.querySelectorAll("#operationButton");
 const numberButtons = document.querySelectorAll("#numberButton");
-const calcDisplay = document.querySelector("#calcDisplay");
-const resetButton = document.querySelector("#resetButton");
+const addBtn = document.querySelector("#addbtn");
+const subtractBtn = document.querySelector("#subtractBtn");
+const multiplyBtn = document.querySelector("#multiplyBtn");
+const divideBtn = document.querySelector("#divideBtn");
+const operateBtn = document.querySelector("#operateBtn");
 
-//variables for updating display
+const resetButton = document.querySelector("#resetButton");
+const mainDisplay = document.querySelector("#mainDisplay");
+
+// variables for updating display
+//? should these variables be looped over somehow?
 let numOne = 0;
 let numTwo = 0;
+let realNum1 = parseInt(numOne);
+let realNum2 = parseInt(numTwo);
+let operator = "";
+let operationPending = false;
 
-//? operator = add(...args))
-let operator;
+//? let the following function only return a number and hanle the logic elsewhere
+function numberBtn() {
+  numberButtons.forEach((button) => {
+    return button.addEventListener("click", () => {
+      const number = button.innerHTML;
+      const actualNumber = parseInt(number);
 
+      if (!operationPending) {
+        mainDisplay.value += actualNumber;
+      } else if (numOne !== 0 && mainDisplay.value !== "") {
+        operationPending = false;
+        mainDisplay.value = "";
+        mainDisplay.value += actualNumber;
+      }
 
-resetButton.addEventListener("click", reset)
+      //console.log(mainDisplay.value, numOne, operator, numTwo);
+      return actualNumber;
+    });
+  });
+}
+numberBtn();
 
-operationButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const operation = button.innerHTML;
+addBtn.addEventListener("click", () => {
+  add();
+});
 
+operateBtn.addEventListener("click", () => {
+  operate();
+});
 
-  })
-})
+resetButton.addEventListener("click", reset);
 
-numberButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const number = button.innerHTML;
-
-    if (number === "0") {
-      calcDisplay.value += 0
-    } else if (number === "1") {
-      calcDisplay.value += 1
-    } else if (number === "2") {
-      calcDisplay.value += 2
-    } else if (number === "3") {
-      calcDisplay.value += 3
-    } else if (number === "4") {
-      calcDisplay.value += 4
-    } else if (number === "5") {
-      calcDisplay.value += 5
-    } else if (number === "6") {
-      calcDisplay.value += 6
-    } else if (number === "7") {
-      calcDisplay.value += 7
-    } else if (number === "8") {
-      calcDisplay.value += 8
-    } else if (number === "9") {
-      calcDisplay.value += 9
-    }
-
-  })
-})
-
-
+function display(actualNumber) {}
 
 // the operator button takes the number value in the display and stores it in a global variable or array
 // operate(storedNumA, storedNumB)
 // function operate(...args) {arrayFrom(args)}
 
+function add(a, b, actualNumber) {
+  operator = "add";
+  operationPending = true;
 
-function add(a, b) {
-  return a + b;
+  if (numOne === 0) {
+    numOne = mainDisplay.value;
+  } else if (numOne !== 0) {
+    numTwo = mainDisplay.value;
+  }
+
+  console.log(operationPending);
+  console.log(numOne, numTwo)
+  return (sum = a + b);
 }
 
 //subtract
@@ -77,14 +86,37 @@ function divide(a, b) {
 }
 
 function reset() {
-  calcDisplay.value = ""
+  mainDisplay.value = "";
+  numOne = 0;
+  numTwo = 0;
+  operate = "";
+  let operationPending = false;
 }
 
 //do the thing
-function operate(a, b) {}
+function operate() {
+  numTwo = mainDisplay.value;
 
-function display() {
+  let realNum1 = parseInt(numOne);
+  let realNum2 = parseInt(numTwo);
 
+  let sum = 0;
+  operationPending = false;
+
+  if ((operate = "add" && numOne > 0 && numTwo > 0)) {
+    sum = add(realNum1, realNum2);
+    mainDisplay.value = sum;
+
+    numOne = sum;
+    numTwo = 0;
+
+    console.log(`${realNum1} + ${realNum2} = ${sum}`);
+    console.log(`numOne set to sum: ${sum}, numTwo set to ${numTwo}`);
+
+    operationPending = false;
+    console.log(operationPending);
+  }
+  return sum;
 }
 
 //number buttons are displayed in the input field and stored somewhere (array/variable) when an operator is clicked.
